136cb1a0cf673638f6e64d24ce525acf2e8c882a
leetecea <leeleticiac@gmail.com>
2022-07-12 14:15:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Morango from "./img/morango.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = {
+        nome: "Francisca",
+        idade: 28,
+        comida: "batata frita",
+        musicas: [ "Outra vida - Armandinho", "Never be the same - Camila Cabello", "A balada de Tim Bernardes - Tim Bernardes"]
+    }
+
+    render(){
+        return(
+            <>
+            <main>
+            <h1>Meu nome é {this.state.nome}</h1>
+            <h2>Tenho {this.state.idade} anos</h2>
+            <h3>Minha comida favorita é {this.state.comida}</h3>
+            <h3>Minhas músicas favoritas atualmente são:</h3>
+            <ul>
+                <li>{this.state.musicas[0]}</li>
+                <li>{this.state.musicas[1]}</li>
+                <li>{this.state.musicas[2]}</li>
+            </ul>
+            <figure>
+                <img src={Morango} alt="Morango" />
+            </figure>
+            </main>
+            </>
+        )
+    }
 }
 
 export default App;
